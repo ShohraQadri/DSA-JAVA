1ebf1b879a21453042d490c8b25b8cc151ff5842
Shohra Qadri <qadrishohra@gmail.com>
2025-02-01 19:33:04 +0530

    push the code

--- Java-program/loops.java
+++ Java-program/loops.java
@@ -147,13 +147,183 @@ public class loops {
 
         // ++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-        int n = 5;
+        // int n = 5;
+        // for (int i = 1; i <= number; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(n);
+        // }
+        // System.out.println();
+        // n--;
+        // }
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // 1
+        // 1 2
+        // 1 2 3
+        // 1 2 3 4
+        // 1 2 3 4 5
+        // 1 2 3 4 5
+        // 1 2 3 4
+        // 1 2 3
+        // 1 2
+        // 1
+
+        // for (int i = 1; i <= number; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+        // for (int i = number; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // 1 2 3 4 5 6
+        // 2 3 4 5 6
+        // 3 4 5 6
+        // 4 5 6
+        // 5 6
+        // 6
+
+        // for (int i = 1; i <= number; i++) {
+        // for (int j = i; j <= number; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // 1-------1
+        // -2-----2
+        // --3---3
+        // ---4-4
+        // ----5
+        // ---4-4
+        // --3---3
+        // -2-----2
+        // 1-------1
+
+        // int leftspace = 0;
+        // int middlespace = 1 + (number - 2) * 2;
+
+        // for (int i = 1; i <= number; i++) {
+        // for (int j = 1; j <= leftspace; j++) {
+        // System.out.print(" ");
+        // }
+        // System.out.print(i);
+
+        // for (int j = 1; j <= middlespace; j++) {
+        // System.out.print(" ");
+        // }
+        // if (i != number)
+        // System.out.print(i);
+        // leftspace = leftspace + 1;
+        // middlespace = middlespace - 2;
+        // System.out.println();
+        // }
+        // // lowe part
+        // leftspace = number - 2;
+        // middlespace = 1;
+        // for (int i = number - 1; i >= 1; i--) {
+        // for (int j = 1; j <= leftspace; j++) {
+        // System.out.print(" ");
+        // }
+        // System.out.print(i);
+
+        // for (int j = 1; j <= middlespace; j++) {
+        // System.out.print(" ");
+        // }
+
+        // System.out.print(i);
+        // leftspace = leftspace - 1;
+        // middlespace = middlespace + 2;
+        // System.out.println();
+        // }
+
+        // +++++++++++++++++++++++++++++++++++++++++++
+
+        // ----*****
+        // ---*---*
+        // --*---*
+        // -*---*
+        // *****
+
+        // int space = number - 1;
+        // for (int i = 1; i <= number; i++) {
+        // for (int j = 1; j <= space; j++) {
+        // System.out.print(" ");
+        // }
+        // if (i == 1 || i == number) {
+        // for (int j = 1; j <= number; j++) {
+        // System.out.print("*");
+        // }
+        // } else {
+        // System.out.print("*");
+        // for (int j = 1; j <= number - 2; j++) {
+        // System.out.print(" ");
+        // }
+
+        // System.out.print("*");
+        // }
+        // space = space - 1;
+        // System.out.println();
+        // }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // **********
+        // ****--****
+        // ***----***
+        // **------**
+        // *--------*
+        // *--------*
+        // **------**
+        // ***----***
+        // ****--****
+        // **********
+
+        int star = number;
+        int space = 0;
+
         for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(n);
+            for (int j = 1; j <= star; j++) {
+                System.out.print("*");
             }
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= star; j++) {
+                System.out.print("*");
+            }
+            star = star - 1;
+            space = space + 2;
             System.out.println();
-            n--;
+        }
+
+        // lower part
+
+        space = space - 2;
+        star = star + 1;
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= star; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= star; j++) {
+                System.out.print("*");
+            }
+            star = star + 1;
+            space = space - 2;
+            System.out.println();
         }
     }
 
