0d9e3e3c86eefa611260526707d34a28e8968ac5
Shohra Qadri <qadrishohra@gmail.com>
2025-02-05 00:47:24 +0530

    add the two-d array and array question pdf

--- Java-program/basictwodarray.java
+++ Java-program/basictwodarray.java
@@ -112,6 +112,200 @@ public class basictwodarray {
         // 15
         // 15
 
+        // System.out.println("enter the row number");
+        // int row = Sc.nextInt();
+        // System.out.println(("enetr the column number"));
+        // int col = Sc.nextInt();
+        // System.out.println("enter the element");
+        // int myarr[][] = new int[row][col];
+
+        // for (int i = 0; i < row; i++) {
+        // for (int j = 0; j < col; j++) {
+        // myarr[i][j] = Sc.nextInt();
+        // }
+        // }
+
+        // int sumL = 0;
+        // int sumR = 0;
+
+        // for (int i = 0; i < col; i++) {
+        // for (int j = 0; j < row; j++) {
+        // if (i == j) {
+        // sumL += myarr[i][j];
+        // }
+        // if ((i + j) == row - 1) {
+        // sumR += myarr[i][j];
+        // }
+        // }
+
+        // }
+        // System.out.println(sumL);
+        // System.out.println(sumR);
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // reverse the first row and last row
+        // input 3 3
+        // 1 2 3
+        // 4 5 6
+        // 7 8 9
+
+        // output
+        // 3 2 1
+        // 4 5 6
+        // 9 8 7
+
+        // System.out.println("enter the row number");
+        // int row = Sc.nextInt();
+        // System.out.println(("enetr the column number"));
+        // int col = Sc.nextInt();
+        // System.out.println("enter the element");
+        // int myarr[][] = new int[row][col];
+
+        // for (int i = 0; i < row; i++) {
+        // for (int j = 0; j < col; j++) {
+        // myarr[i][j] = Sc.nextInt();
+        // }
+        // }
+        // int j = 0;
+        // for (int i = 0; i < row; i++) {
+        // int temp = myarr[i][j];
+        // myarr[i][j] = myarr[i][row - 1 - j];
+        // myarr[i][row - 1 - j] = temp;
+
+        // j++;
+        // }
+
+        // for (int i = 0; i < row; i++) {
+        // for (j = 0; j < col; j++) {
+        // System.out.print(myarr[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // pritn the upper triangel in the array
+        // input
+        // 1 2 3 4
+        // 5 6 7 8
+        // 9 1 2 3
+        // 4 5 6 7
+
+        // output
+        // 1 2 3 4
+        // 6 7 8
+        // 2 3
+        // 7
+
+        // System.out.println("enter the row number");
+        // int row = Sc.nextInt();
+        // System.out.println(("enetr the column number"));
+        // int col = Sc.nextInt();
+        // System.out.println("enter the element");
+        // int myarr[][] = new int[row][col];
+
+        // for (int i = 0; i < row; i++) {
+        // for (int j = 0; j < col; j++) {
+        // myarr[i][j] = Sc.nextInt();
+        // }
+        // }
+
+        // for (int i = 0; i < row; i++) {
+        // for (int j = 0; j < col; j++) {
+        // if (i <= j) {
+        // System.out.print(myarr[i][j] + " ");
+        // } else {
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println();
+        // }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // pritn the lower triangel in the array
+        // input
+        // 1 2 3 4
+        // 5 6 7 8
+        // 9 1 2 3
+        // 4 5 6 7
+
+        // output
+        // 1
+        // 5 6
+        // 9 1 2
+        // 4 5 6 7
+
+        // System.out.println("enter the row number");
+        // int row = Sc.nextInt();
+        // System.out.println(("enetr the column number"));
+        // int col = Sc.nextInt();
+        // System.out.println("enter the element");
+        // int myarr[][] = new int[row][col];
+
+        // for (int i = 0; i < row; i++) {
+        // for (int j = 0; j < col; j++) {
+        // myarr[i][j] = Sc.nextInt();
+        // }
+        // }
+
+        // for (int i = 0; i < row; i++) {
+        // for (int j = 0; j < col; j++) {
+        // if (i >= j) {
+        // System.out.print(myarr[i][j] + " ");
+        // } else {
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println();
+        // }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // find sum of upper triangular matrix
+        // input
+        // 1 2 3 -----2 3
+        // 4 5 6 -------6
+        // 7 8 9
+
+        // output
+        // 11
+
+        // System.out.println("enter the row number");
+        // int row = Sc.nextInt();
+        // System.out.println(("enetr the column number"));
+        // int col = Sc.nextInt();
+        // System.out.println("enter the element");
+        // int myarr[][] = new int[row][col];
+
+        // for (int i = 0; i < row; i++) {
+        // for (int j = 0; j < col; j++) {
+        // myarr[i][j] = Sc.nextInt();
+        // }
+        // }
+        // int sum = 0;
+        // for (int i = 0; i < row; i++) {
+        // for (int j = 0; j < col; j++) {
+        // if (i < j) {
+        // sum = sum + myarr[i][j];
+        // }
+        // }
+        // }
+        // System.out.print(sum + " ");
+
+        // first matrix
+        // 1 2 3 - - - - - - - 1 2 3
+        // 4 5 6 - - - + - - - 4 5 6
+        // 7 8 9 - - - - - - - 7 8 9
+
+        // out put
+        // 2 4 6
+        // 8 10 12
+        // 14 16 18
+
+        // add two matrix
+        System.out.println("for matrix 1");
         System.out.println("enter the row number");
         int row = Sc.nextInt();
         System.out.println(("enetr the column number"));
@@ -124,23 +318,32 @@ public class basictwodarray {
                 myarr[i][j] = Sc.nextInt();
             }
         }
-
-        int sumL = 0;
-        int sumR = 0;
-
-        for (int i = 0; i < col; i++) {
-            for (int j = 0; j < row; j++) {
-                if (i == j) {
-                    sumL += myarr[i][j];
-                }
-                if ((i + j) == row - 1) {
-                    sumR += myarr[i][j];
-                }
+        System.out.println("for matrix two");
+        System.out.println("enter the row number");
+        int row1 = Sc.nextInt();
+        System.out.println(("enetr the column number"));
+        int col1 = Sc.nextInt();
+        System.out.println("enter the element");
+        int myarr1[][] = new int[row1][col1];
+        int subarray[][] = new int[row][col];
+        for (int i = 0; i < row1; i++) {
+            for (int j = 0; j < col1; j++) {
+                myarr1[i][j] = Sc.nextInt();
             }
-
         }
-        System.out.println(sumL);
-        System.out.println(sumR);
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                subarray[i][j] = myarr[i][j] + myarr[i][j];
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(subarray[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
