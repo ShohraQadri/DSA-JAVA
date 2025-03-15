d9ac638a381c33abbc641c25d0f54f791e0ca12c
Shohra Qadri <qadrishohra@gmail.com>
2025-03-15 21:45:03 +0530

    frequency java code

--- Java-program/Frequency.java
+++ Java-program/Frequency.java
@@ -2,23 +2,29 @@ import java.util.Scanner;
 
 public class Frequency {
     public static void main(String[] arg) {
-        Scanner Sc = new Scanner(System.in);
-        System.out.println("enter the size of array");
-        int number = Sc.nextInt();
 
-        int arr[] = new int[number];
-        for (int i = 0; i < number; i++) {
-            System.out.println("enter element");
-            arr[i] = Sc.nextInt();
-        }
+        // find the frequency of the element
 
-        for (int i = 0; i < number; i++) {
-            int sum = 0;
-            for (int j = i; j < number; j++) {
-                sum += arr[j];
-                System.out.println(i + "--> " + j + " = " + sum);
-            }
-        }
+        // Scanner Sc = new Scanner(System.in);
+        // System.out.println("enter the size of array");
+        // int number = Sc.nextInt();
+
+        // int arr[] = new int[number];
+        // for (int i = 0; i < number; i++) {
+        // System.out.println("enter element");
+        // arr[i] = Sc.nextInt();
+        // }
+
+        // for (int i = 0; i < number; i++) {
+        // int sum = 0;
+        // for (int j = i; j < number; j++) {
+        // sum += arr[j];
+        // System.out.println(i + "--> " + j + " = " + sum);
+        // }
+        // }
+
+        // arraylist question
+
     }
 
 }
